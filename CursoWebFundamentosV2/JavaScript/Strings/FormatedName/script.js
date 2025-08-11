281d4123332a5501ada823c30b22819096cad296
Patrikoviski <shorelinegames@icloud.com>
2025-08-11 20:21:37 -0300

    modified:   CursoWebFundamentosV2/JavaScript/Strings/FormatedName/script.js

--- CursoWebFundamentosV2/JavaScript/Strings/FormatedName/script.js
+++ CursoWebFundamentosV2/JavaScript/Strings/FormatedName/script.js
@@ -1,9 +1,10 @@
 function nameFormated(fullName) {
-    let fullName = fullName
+    fullName = fullName
     let spacePosition = fullName.trim().indexOf(" ")
     let firstName = fullName.slice(0, spacePosition)
     let lastName = fullName.slice(spacePosition)
     return `${lastName}, ${firstName}`
 }
+
 
 console.log(nameFormated("Patrik Grodoviski"))
