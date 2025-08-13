d39cec27d5b742effa11a874d72760f3c48f4af1
Patrikoviski <shorelinegames@icloud.com>
2025-08-13 17:31:59 -0300

    modified:   CursoWebFundamentosV2/JavaScript/Date/birthday/index.html 	modified:   CursoWebFundamentosV2/JavaScript/Date/birthday/js/main.js

--- CursoWebFundamentosV2/JavaScript/Date/birthday/js/main.js
+++ CursoWebFundamentosV2/JavaScript/Date/birthday/js/main.js
@@ -1,4 +1,21 @@
-function quantoFaltaPara(m, d) {
+function quantoFaltaPara(mounth, day) {
+    const today = new Date()
+    const OneDay = 24 * 60 * 60 * 1000
+    let actualYear = today.getFullYear()
+    let birth = new Date(actualYear, mounth - 1, day)
+    let actualDateTS = +today
+    let dateBirthTS = +birth
+    
+    today.setHours(0)
+    today.setMinutes(0)
+    today.setSeconds(0)
+    today.setMilliseconds(0)
 
+    if (dateBirthTS < actualDateTS) {
+        birth.setFullYear(++actualYear)
+        dateBirthTS = +birth
+    }
 
+   let diference = dateBirthTS - actualDateTS
+    return parseInt(diference / OneDay)
 }
