38005e4c4f11987381e89aad1cf97d03dcb7f5d9
Patrikoviski <shorelinegames@icloud.com>
2025-08-20 17:09:00 -0300

    modified:   CursoWebFundamentosV2/JavaScript/Styles/bg_paralax/index.html 	modified:   CursoWebFundamentosV2/JavaScript/Styles/bg_paralax/js/main.js 	new file:   CursoWebFundamentosV2/JavaScript/Styles/showImages/css/main.css 	new file:   CursoWebFundamentosV2/JavaScript/Styles/showImages/imagens/bg-1.jpg 	new file:   CursoWebFundamentosV2/JavaScript/Styles/showImages/imagens/bg-2.jpg 	new file:   CursoWebFundamentosV2/JavaScript/Styles/showImages/imagens/bg-3.jpg 	new file:   CursoWebFundamentosV2/JavaScript/Styles/showImages/imagens/bg-4.jpg 	new file:   CursoWebFundamentosV2/JavaScript/Styles/showImages/imagens/card_01.png 	new file:   CursoWebFundamentosV2/JavaScript/Styles/showImages/imagens/card_02.jpg 	new file:   CursoWebFundamentosV2/JavaScript/Styles/showImages/imagens/card_03.jpg 	new file:   CursoWebFundamentosV2/JavaScript/Styles/showImages/imagens/card_04.jpg 	new file:   CursoWebFundamentosV2/JavaScript/Styles/showImages/imagens/card_05.jpg 	new file:   CursoWebFundamentosV2/JavaScript/Styles/showImages/index.html 	new file:   CursoWebFundamentosV2/JavaScript/Styles/showImages/js/addclass.js 	new file:   CursoWebFundamentosV2/JavaScript/Styles/showImages/js/paralax.js 	modified:   c#/Exercicios/Taxes/Taxes/obj/Debug/net8.0/Taxes.AssemblyInfo.cs 	modified:   c#/Exercicios/Taxes/Taxes/obj/Debug/net8.0/Taxes.AssemblyInfoInputs.cache

--- c#/Exercicios/Taxes/Taxes/obj/Debug/net8.0/Taxes.AssemblyInfo.cs
+++ c#/Exercicios/Taxes/Taxes/obj/Debug/net8.0/Taxes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b30f34a14dae3a848bec0fb6d30f4ffd6c040a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8203d74112f3d25e2cd1a564811b801494e28093")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
