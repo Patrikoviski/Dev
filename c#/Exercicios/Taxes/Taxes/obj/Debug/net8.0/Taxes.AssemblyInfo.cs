d96ec7f021026295359d2596ff9e67d1aa8a6015
Patrikoviski <shorelinegames@icloud.com>
2025-09-09 20:22:27 -0300

    creating json-fake_API

--- c#/Exercicios/Taxes/Taxes/obj/Debug/net8.0/Taxes.AssemblyInfo.cs
+++ c#/Exercicios/Taxes/Taxes/obj/Debug/net8.0/Taxes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eee02c9a572abf879fbaa365e263ad92cbda160")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea109b18a9b9ed2ae441bb735d372a817ea1345b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
