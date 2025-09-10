a7a1c56c4c889fd534fd02e1a02c15a50d6eb11c
Patrikoviski <shorelinegames@icloud.com>
2025-09-10 19:43:34 -0300

    updating json-fake_API main.js

--- CursoWebFundamentosV2/JavaScript/ajax/json-fake_API/script/main.js
+++ CursoWebFundamentosV2/JavaScript/ajax/json-fake_API/script/main.js
@@ -1,3 +1,14 @@
+// let title = document.getElementById("title")
+// let body = document.getElementById("body")
+
+document.querySelector("#registerForm").addEventListener("submit", function(evt){
+    evt.preventDefault()
+
+    postRegister(1)
+})
+
+// Get
+
 async function searchPostByUser(id){
     try {
         const answer = await fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
@@ -21,4 +32,31 @@ searchPostByUser(1)
 
 function showData(data){
     console.log(data)
+}
+
+// Post
+
+async function postRegister(userID){
+    try {
+        const answer = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+            method: 'POST',
+            body: JSON.stringify({
+                title: document.querySelector("#title").value,
+                body: document.querySelector("#body").value,
+                userID
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF=8',
+            },
+        })
+
+        console.log(answer)
+        console.log(answer.status)
+
+        const data = await answer.json()
+        showData(data)
+
+    } catch(e){
+        alert(`Error: ${e}`)
+    }
 }
