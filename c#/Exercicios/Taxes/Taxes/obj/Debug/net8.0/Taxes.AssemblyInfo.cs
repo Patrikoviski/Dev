f0fc15a008212fe7bf4ffd9bfb29e625bf4fd800
Patrikoviski <shorelinegames@icloud.com>
2025-09-12 20:25:35 -0300

    new file:   CursoWebFundamentosV2/Projeto_Final/assets/01.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/02.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/03.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/arrowup.png 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/articles/01.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/articles/02.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/articles/03.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/articles/04.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/articles/05.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/articles/06.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/articles/07.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/articles/08.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/articles/09.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/articles/10.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/articles/11.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/articles/12.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/av01.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/av01.png 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/av02.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/av02.png 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/av03.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/av03.png 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/av04.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/av04.png 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/avbg-01.png 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/c01.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/c02.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/c03.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/c04.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/herobg.jpg 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/logo.png 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/slider/.DS_Store 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/slider/slide-01.png 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/slider/slide-02.png 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/teamsprite.png 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/teamsprite_01.png 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/teamsprite_02.png 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/teamsprite_03.png 	new file:   CursoWebFundamentosV2/Projeto_Final/assets/teamsprite_04.png 	modified:   CursoWebFundamentosV2/Projeto_Final/index.html 	modified:   c#/Exercicios/Taxes/Taxes/obj/Debug/net8.0/Taxes.AssemblyInfo.cs 	modified:   c#/Exercicios/Taxes/Taxes/obj/Debug/net8.0/Taxes.AssemblyInfoInputs.cache

--- c#/Exercicios/Taxes/Taxes/obj/Debug/net8.0/Taxes.AssemblyInfo.cs
+++ c#/Exercicios/Taxes/Taxes/obj/Debug/net8.0/Taxes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a1c56c4c889fd534fd02e1a02c15a50d6eb11c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd28f6d8dc4192ff24ad948c3b7fdcde0c915d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
