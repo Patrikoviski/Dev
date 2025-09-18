00eb17f6797f2cd963cd54d36c9e12e9825684c1
Patrikoviski <shorelinegames@icloud.com>
2025-09-18 18:55:22 -0300

    failed to corret a bug at quote-container

--- CursoWebFundamentosV2/Projeto_Final/script/main.js
+++ CursoWebFundamentosV2/Projeto_Final/script/main.js
@@ -1,55 +1,20 @@
-document.querySelector(".no-js").remove
-document.body.classList = "js"
-document.querySelector("span").style = ""
-let slideshow = document.querySelector(".slideshow")
-let quote_slideshow = document.querySelector(".quote-slideshow")
-let quote_slideshow_01 = `<figure style="width: 70%;">
-                    <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam maiores minima sapiente laboriosam repellat error.</h2>
-                    <figcaption>Lorem ipsum dolor sit amet.</figcaption>
-                </figure>`
-let quote_slideshow_02 = `<figure>
-                    <h2>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex saepe cupiditate fuga. Dolores, officiis quas!</h2>
-                    <figcaption>Lorem ipsum dolor sit.</figcaption>
-                </figure>`           
+(function(){
+  document.querySelector(".no-js").remove
+  document.body.classList = "js"
 
-quote_slideshow.innerHTML = `<span class="prev"><i class="fas fa-angle-left"></i></span>${quote_slideshow_01}<span class="next"><i class="fas fa-angle-right "></i></span>`
+  console.log("abacaxi")
 
+  var carouselImgs = new Carousel({
+    container: '.laptop-slider .slideshow',
+    itens: 'figure',
+    btnPrev: '.prev',
+    btnNext: '.next'
+  })
 
-function nextSlide() {
-  let currentImage = slideshow.querySelector("figure img")
-  let currentSrc = currentImage.src
-  let count = parseInt(currentSrc.slice(-5, -4))
-  
-  if (count < 2) {
-    count++
-  } else {
-    count = 1
-  }
-  
-  currentImage.src = `assets/slider/slide-0${count}.png`
-}
-
-function prevSlide() {
-    let currentImage = slideshow.querySelector("figure img")
-    let currentSrc = currentImage.src
-    let count = parseInt(currentSrc.slice(-5, -4))
-    
-    
-    if (count > 1) { 
-        count--;
-    } else {
-        count = 2;
-    }
-    
-    currentImage.src = `assets/slider/slide-0${count}.png`
-}
-
-
-slideshow.innerHTML = '<span class="prev"><i class="fa-solid fa-angle-left"></i></span><figure><img src="assets/slider/slide-01.png" alt=""></figure><span class="next"><i class="fa-solid fa-angle-right"></i></span>'
-slideshow.classList.add("slideshowJS")
-
-const leftArrow = document.querySelector(".prev")
-const rightArrow = document.querySelector(".next")
-
-rightArrow.addEventListener("click", nextSlide)
-leftArrow.addEventListener("click", prevSlide)
+  var carouselQuotes = new Carousel({
+        container: '.quote-slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next'
+    })
+})()
--- c#/Exercicios/Taxes/Taxes/obj/Debug/net8.0/Taxes.AssemblyInfo.cs
+++ c#/Exercicios/Taxes/Taxes/obj/Debug/net8.0/Taxes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6269e8f4b69e29c24b4f2470ae9a2f1e3a8138d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16ed3dd8fb391a24823ffdd371b213c5f2848a6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
