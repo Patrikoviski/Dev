8cd1f8cfd256d46a48993cd4dbba91468386e40b
Patrikoviski <shorelinegames@icloud.com>
2025-09-19 21:26:44 -0300

    updating index.html main.css from Projeto_Final_Serliv

--- c#/Exercicios/Taxes/Taxes/obj/Debug/net8.0/Taxes.AssemblyInfo.cs
+++ c#/Exercicios/Taxes/Taxes/obj/Debug/net8.0/Taxes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16ed3dd8fb391a24823ffdd371b213c5f2848a6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb1239d35425a3dd6463740bda6c96aae562bdae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Taxes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
