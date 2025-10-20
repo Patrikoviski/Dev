cf9b7d03c1989edae86b64c1d2efd70304976cbd
Patrikoviski <shorelinegames@icloud.com>
2025-10-20 18:51:08 -0300

    adding DobrarValor directory exercise

---
+++ Javascript/Estagiario-Junior/Arrays e Loops/DobrarValor/script.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, 5, 6]
+
+const dobro = (array) => array.map(e => e * 2)
+
+
+// function dobro(array){
+//     return array.map(e => {
+//         return e * 2
+//     })
+// }
+
+console.log(numbers)
+console.log(dobro(numbers))
