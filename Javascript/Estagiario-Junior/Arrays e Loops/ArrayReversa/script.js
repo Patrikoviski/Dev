b84db797b65133327c724903e3a86747d8e72fda
Patrikoviski <shorelinegames@icloud.com>
2025-10-20 14:50:02 -0300

    adding ArrayReversa directory exercise

---
+++ Javascript/Estagiario-Junior/Arrays e Loops/ArrayReversa/script.js
@@ -0,0 +1,14 @@
+const names = [
+    "Kamily <3",
+    "patrik",
+    "toby",
+    "jose",
+    "brino"
+]
+
+
+function reverseArray(array){
+    return array.toReversed().toString()
+}
+
+console.log(reverseArray(names))
