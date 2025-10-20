8a5d0e5a9dd952eabdce6f90444e6b221eef283c
Patrikoviski <shorelinegames@icloud.com>
2025-10-20 20:24:24 -0300

    adding ConcatenarArrays directory

---
+++ Javascript/Estagiario-Junior/Arrays e Loops/ConcatenarArrays/script.js
@@ -0,0 +1,10 @@
+const numbers1 = [1, 2, 3, 4, 5, 6]
+const numbers2= [7, 8, 9, 10]
+
+
+function concatenar(array1, array2){
+    let novoArray = array1.concat(...array2)
+    return novoArray.toString()
+}
+
+console.log(concatenar(numbers1, numbers2))
