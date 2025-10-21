62e54574210f092eee0d6c89ddc44113efa223e6
Patrikoviski <shorelinegames@icloud.com>
2025-10-21 19:36:56 -0300

    adding CompararArrays directory exercise

---
+++ Javascript/Estagiario-Junior/Logica e Desafios/CompararArrays/script.js
@@ -0,0 +1,9 @@
+const array1 = [1, 1, 1, 1, 1]
+const array2 = [1, 1, 1, 1, 1]
+const array3 = [1, 1, 1, 2, 1]
+
+
+
+const iguais = (array01, array02) => array01.toString() === array02.toString() ? true : false
+
+console.log(iguais(array1, array2))
