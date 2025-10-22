c2e57051a7db8ccb88cf212c04e49dd16d8eaa39
Patrikoviski <shorelinegames@icloud.com>
2025-10-22 11:52:34 -0300

    creating Front-End and Estagio-junior directory and adding CartaoDePerfilSimples directory exercise

---
+++ Front-End/Estagio-Junior/CartaoDePerfilSimples/script/main.js
@@ -0,0 +1,6 @@
+const body = document.querySelector("body")
+const card = document.querySelector(".card")
+
+body.addEventListener("mouseenter", () => {
+    card.style = "transform: scale(1.1);"
+})
--- Javascript/Estagiario-Junior/Logica e Desafios/EncontrarSegundoMaior/script.js
+++ Javascript/Estagiario-Junior/Logica e Desafios/EncontrarSegundoMaior/script.js
@@ -1,5 +1,7 @@
 const numbers = [10, 2, 8, 4]
 
+// const segundoMaior = (array) => [...array].sort((a, b) => b - a)
+
 const segundoMaior = (array) => {
     const sortedArray = [...array].sort((a, b) => b - a)
     return sortedArray[1]
